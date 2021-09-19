4a2f66d18d1da18808507dbbead9541573930dac
Aleksandr Utkin <byllipyt@icloud.com>
2021-09-19 10:01:26 +0300

    Initial Commit

---
+++ SUIEducation/SUIEducationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIEducationApp.swift
+//  SUIEducation
+//
+//  Created by Александр Уткин on 19.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SUIEducationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
